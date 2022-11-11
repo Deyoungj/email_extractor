cd4d6f6389f903b39310094f082c88bdafe6d9c6
Deyoungj <emmanuelonyibor39@gmail.com>
2022-11-11 22:01:19 +0100

    sidebar

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests, re
+from bs4 import BeautifulSoup
+
 
 
 EMAIL_REX = r'[\w\.]+@[\w\.]+'
 
 url = ''
 
-r = requests.get()
+r = requests.get("https://www.google.com/search?q=requests+hhas+no+attribute+status&oq=requests+hhas+no+attribute+status&aqs=chrome..69i57j0i22i30l4.38788j0j4&sourceid=chrome&ie=UTF-8")
 
 
-print(r.status)
+print(BeautifulSoup(r.text).prettify())
