29eb9abc427a3f8ca1813ec90e3a790f5ef04f86
Deyoungj <emmanuelonyibor39@gmail.com>
2022-12-05 15:30:12 +0100

    adding gui

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from collections import deque
 import urllib
 
-def process_links(url: str , length: int = 10, email_type: str = ".") -> list:
+def process_link(url: str , length: int , email_type: str) -> list:
     
     unprocessed_links = deque([url])
 
@@ -37,7 +37,7 @@ def process_links(url: str , length: int = 10, email_type: str = ".") -> list:
         except:
             pass
 
-        print(f"[{count}]**  processing link: [{url}] **status: [{response.status_code}] ")
+        print(f"--------[{count}]**  processing link: [{url}] **status: [{response.status_code}] \n")
 
 
         new_email = re.findall(r"[a-zA-Z0-9\.\-+_]+@[a-zA-Z0-9\.\-+_]+\.[a-z]+", response.text, re.I)
@@ -63,28 +63,51 @@ def process_links(url: str , length: int = 10, email_type: str = ".") -> list:
 
 
 
-def main(url: str):
+def main(url: list, length: int = 5, email_type: str = "."):
 
     
+    # all = "all"
+    # hotmail = "hotmail"
+    # gmail = "gmail"
+    # outlook = "outlook"
 
     # queue of urls to be crawled
-    unprocessed_urls = deque([])
+    unprocessed_urls = deque(url)
 
     # set of already crawled urls for email
-    processed_urls = set()
+    processed_urls = []
 
-    mails = process_links(url)
+    # all emails goten from the links
+    all_emails = set()
+    count = 0
+
+    while unprocessed_urls:
+
+        url = unprocessed_urls.popleft()
+        processed_urls.append(url)
+
+        print(" processing url: %s" % url + "\n \n")
+        count += 1
+        mails = process_link(url, length, email_type)
+
+        for mail in mails:
+            all_emails.add(mail)
+
 
     for mail in mails :
         with open("mails.txt", "a") as f:
             f.write(mail+"\n")
     
-    print("files has been added to mail.txt")
+    print("all emails has been saved ")
     
 
 
 
-main("https://www.google.com/search?q=truck+contract+of+kent+email+%22%40hotmail.com%22+%40outlook.com%22-issuu+-scam&oq=truck+contract+of+kent+email+%22%40hotmail.com%22+%40outlook.com%22-issuu+-scam&aqs=chrome..69i57.120518j0j7&sourceid=chrome&ie=UTF-8")
+main([
+    "https://www.google.com/search?q=truck+contract+of+kent+email+%22%40hotmail.com%22+%40outlook.com%22-issuu+-scam&oq=truck+contract+of+kent+email+%22%40hotmail.com%22+%40outlook.com%22-issuu+-scam&aqs=chrome..69i57.120518j0j7&sourceid=chrome&ie=UTF-8",
+
+    "https://www.google.com/search?q=company+real+estate+washington+%40hotmail.com&oq=compa&aqs=chrome.0.69i59j69i57j69i59l2j0i67l3j0i433i512j0i67l2.10644j0j7&sourceid=chrome&ie=UTF-8"
+])
 
 
 
